3fa5114ef543b4f04b70393cdaf874912249a0f7
D.Lin <lindaqi087@gmail.com>
2023-04-29 02:49:56 +0200

    add itemFilter component

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
     </v-container>
     <v-main>
       <CreateItem></CreateItem>
+      <ItemFilter v-model:itemFilter="itemFilter"></ItemFilter>
       <ToDoItemVue
-        v-for="toDoItem in toDoArray"
+        v-for="toDoItem in filteredArray"
         :key="toDoItem.timeStamp"
         :toDoItem="toDoItem"
       >
@@ -19,6 +20,7 @@
 import { computed, defineComponent, ref, type ComputedRef } from 'vue';
 
 import CreateItem from './components/CreateItem/CreateItem.vue';
+import ItemFilter from './components/ItemFilter/ItemFilter.vue';
 import ToDoItemVue from './components/ToDoItem/ToDoItem.vue';
 
 import type { ToDoItem } from './interfaces';
@@ -28,14 +30,28 @@ export default defineComponent({
   name: 'App',
   components: {
     CreateItem,
+    ItemFilter,
     ToDoItemVue,
   },
   setup() {
     const store = useStore();
     const toDoArray: ComputedRef<ToDoItem[]> = computed(() => store.getters.getToDoArray);
+    const itemFilter = ref('all');
+
+    const filteredArray: ComputedRef<ToDoItem[]> = computed(() => {
+      let isChecked = itemFilter.value;
+      if(isChecked === 'done') {
+        return toDoArray.value.filter(item => item.itemStatus === true);
+      } else if(isChecked === 'undone') {
+        return toDoArray.value.filter(item => item.itemStatus === false);
+      } else {
+        return toDoArray.value;
+      }
+    });
 
     return {
-      toDoArray,
+      itemFilter,
+      filteredArray
     };
   }
 });
---
+++ src/components/ItemFilter/ItemFilter.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-radio-group v-model="isChecked" inline @change="changeItemFilter">
+      <v-radio label="All" color="info" value="all"></v-radio>
+      <v-radio label="Done" color="info" value="done"></v-radio>
+      <v-radio label="Undone" color="info" value="undone"></v-radio>
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs } from "vue";
+
+export default defineComponent ({
+  name: 'ItemFilter',
+  props: {
+    itemFilter: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, {emit}) {
+    const itemFilter = toRefs(props).itemFilter;
+    const isChecked = ref(itemFilter.value);
+    
+    const changeItemFilter = () => {
+      emit('update:itemFilter', isChecked.value);
+    };
+
+    return {
+      isChecked,
+      changeItemFilter
+    };
+  }
+});
+</script>
+
+<style>
+
+</style>
